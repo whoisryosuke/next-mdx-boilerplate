4ff1d4b50fa7cb274695db35af3189988cb86f94
Ryosuke <ryosuke.san.hana@gmail.com>
2020-04-09 11:38:56 -0700

    :zap: Added header to layout

---
+++ components/Header.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Box, Heading, Flex, Text, Button } from '@chakra-ui/core'
+
+const MenuItems = ({ children }) => (
+  <Text mt={{ base: 4, md: 0 }} mr={6} display="block">
+    {children}
+  </Text>
+)
+
+const Header = (props) => {
+  const [show, setShow] = React.useState(false)
+  const handleToggle = () => setShow(!show)
+
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1.5rem"
+      bg="teal.500"
+      color="white"
+      {...props}
+    >
+      <Flex align="center" mr={5}>
+        <Heading as="h1" size="lg" letterSpacing={'-.1rem'}>
+          Chakra UI
+        </Heading>
+      </Flex>
+
+      <Box display={{ sm: 'block', md: 'none' }} onClick={handleToggle}>
+        <svg
+          fill="white"
+          width="12px"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+      </Box>
+
+      <Box
+        display={{ sm: show ? 'block' : 'none', md: 'flex' }}
+        width={{ sm: 'full', md: 'auto' }}
+        alignItems="center"
+        flexGrow={1}
+      >
+        <MenuItems>Docs</MenuItems>
+        <MenuItems>Examples</MenuItems>
+        <MenuItems>Blog</MenuItems>
+      </Box>
+
+      <Box
+        display={{ sm: show ? 'block' : 'none', md: 'block' }}
+        mt={{ base: 4, md: 0 }}
+      >
+        <Button bg="transparent" border="1px">
+          Create account
+        </Button>
+      </Box>
+    </Flex>
+  )
+}
+
+export default Header
--- layouts/AboutPage.jsx
+++ layouts/AboutPage.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import { Box } from '@chakra-ui/core'
+import Header from '../components/Header'
 
 export default function AboutPage({ children }) {
-  return <div style={{ textAlign: 'center' }}>{children}</div>
+  return (
+    <main>
+      <Header />
+      <Box p={3} textAlign="center">
+        {children}
+      </Box>
+    </main>
+  )
 }
