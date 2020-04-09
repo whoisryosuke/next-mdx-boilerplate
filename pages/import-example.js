d31c8b7b8257c384db0b40ec070f25245637549e
Ryosuke <ryosuke.san.hana@gmail.com>
2020-04-08 17:21:46 -0700

    :sparkles: Import MDX as component

---
+++ pages/import-example.js
@@ -0,0 +1,12 @@
+import About from "./about.mdx"
+
+const TestPage = () => {
+
+    return (
+        <div>
+            <div>Test</div>
+            <About />
+        </div>
+    )
+}
+export default TestPage
